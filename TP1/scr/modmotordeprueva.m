ab3c26967cf99fcd24e7e9cadf0a90bcbf033390
jacquelinebazoalto-netizen <jacqueline.bazoalto@mi.unc.edu.ar>
2026-04-19 23:27:11 -0300

    Update modmotordeprueva.m

--- TP1/scr/modmotordeprueva.m
+++ TP1/scr/modmotordeprueva.m
@@ -1,1 +1,18 @@
+function [X]=modmotorprueba(t_etapa, xant, accion)
+Laa= 1e-04; J=1e-04;Ra=23;B=0;Ki= 0.7;Km=0.15;
+Va=accion(1);
+Tl=accion(2);
+h=10e-7;
+ia=xant(1);
+w= xant(2);
+tita=xant(3);
 
+for ii=1:t_etapa/h
+  iap=(-Ra/Laa)*ia -(Km/Laa)*w + (1/Laa)*Va;
+  wp=(Ki/J)*ia -(B/J)*w - (1/J)*Tl;
+
+  ia=ia+h*iap;
+  w = w + h*wp;
+  tita=tita+h*w;
+end
+X=[ia, w, tita];
